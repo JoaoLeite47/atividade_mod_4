683f340ec55f804656c8b9aae85c7ca75cd9938e
JoaoLeite47 <StalkerJJ@outlook.com>
2022-06-21 21:09:18 -0300

    feat(atv1 e atv2)

--- index.ts
+++ index.ts
@@ -2,55 +2,126 @@ interface User {
   name: string;
   age: number;
   occupation: string;
-  role?: string;
 }
 
-interface Admin {
-  name: string;
-  age: number;
-  role: string;
-  occupation?: string;
-}
-
-export type Person = User | Admin;
-
-export const persons: Person[] = [
+export const users: User[] = [
   {
-    name: "Max Mustermann",
+    name: "Wilker",
     age: 25,
-    occupation: "Chimney sweep",
+    occupation: "Backend developer",
   },
   {
-    name: "Jane Doe",
-    age: 32,
-    role: "Administrator",
-  },
-  {
-    name: "Kate Müller",
+    name: "Bob",
     age: 23,
-    occupation: "Astronaut",
-  },
-  {
-    name: "Bruce Willis",
-    age: 64,
-    role: "World saver",
-  },
-  {
-    name: "Jaozin",
-    age: 21,
-    occupation: "Vulgo dublê",
+    occupation: "Product Manager",
   },
 ];
 
-export function logPerson(user: Person) {
-  let additionalInformation: string | undefined;
-  if (user.role) {
-    additionalInformation = user.role;
-  } else if (user.occupation) {
-    additionalInformation = user.occupation;
-  }
-  console.log(` - ${user.name}, ${user.age}, ${additionalInformation}`);
+export function logPerson(user: User) {
+  console.log(` - ${user.name}, ${user.age}`);
 }
 
-console.log("Users: ");
-persons.forEach(logPerson);
+console.log("Users:");
+users.forEach(logPerson);
+///////////////////////////////////////////// ATV 2
+
+// interface User{
+//   name: string;
+//   age: number;
+//   occupation?: string;
+// }
+
+// interface Admin {
+//   name: string;
+//   age: number;
+//   role?: string;
+// }
+
+// export type Person = User | Admin;
+
+// export const persons: Person[]  = [
+//   {
+//     name: "Max Mustermann",
+//     age: 25,
+//     occupation: "Chimney sweep",
+//   },
+//   {
+//     name: "Jane Doe",
+//     age: 32,
+//     role: "Administrator",
+//   },
+//   {
+//     name: "Kate Müller",
+//     age: 23,
+//     occupation: "Astronaut",
+//   },
+//   {
+//     name: "Bruce Willis",
+//     age: 64,
+//     role: "World saver",
+//   },
+// ];
+
+// export function logPerson(user: Person) {
+//   console.log(` - ${user.name}, ${user.age}`);
+// }
+
+// persons.forEach(logPerson);
+/////////////////////////////////////////////////////// ATV 3
+
+// interface User {
+//   name: string;
+//   age: number;
+//   occupation: string;
+//   role?: string;
+// }
+
+// interface Admin {
+//   name: string;
+//   age: number;
+//   role: string;
+//   occupation?: string;
+// }
+
+// export type Person = User | Admin;
+
+// export const persons: Person[] = [
+//   {
+//     name: "Max Mustermann",
+//     age: 25,
+//     occupation: "Chimney sweep",
+//   },
+//   {
+//     name: "Jane Doe",
+//     age: 32,
+//     role: "Administrator",
+//   },
+//   {
+//     name: "Kate Müller",
+//     age: 23,
+//     occupation: "Astronaut",
+//   },
+//   {
+//     name: "Bruce Willis",
+//     age: 64,
+//     role: "World saver",
+//   },
+//   {
+//     name: "Jaozin",
+//     age: 21,
+//     occupation: "Vulgo dublê",
+//   },
+// ];
+
+// export function logPerson(user: Person) {
+//   let additionalInformation: string | undefined;
+//   if (user.role) {
+//     additionalInformation = user.role;
+//   } else if (user.occupation) {
+//     additionalInformation = user.occupation;
+//   }
+//   console.log(` - ${user.name}, ${user.age}, ${additionalInformation}`);
+// }
+
+// console.log("Users: ");
+// persons.forEach(logPerson);
